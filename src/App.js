c1e29d42c64414194d5909e759e92bf8cdde3959
it-tsumugi <tsumugi.it@gmail.com>
2021-06-15 00:53:30 +0900

    Router テスト

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Link} from "react-router-dom";
+import {BrowserRouter, Link, Switch, Route} from "react-router-dom";
 
 import Home from "./component/pages/Home";
 import Profile from "./component/pages/Profile";
@@ -8,13 +8,37 @@ import Contact from "./component/pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Profile />
-      <Skill />
-      <Product />
-      <Contact />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/component/pages/Home">Home</Link>
+        <br />
+        <Link to="/component/pages/Profile">Profile</Link>
+        <br />
+        <Link to="/component/pages/Skill">Skill</Link>
+        <br />
+        <Link to="/component/pages/Product">Product</Link>
+        <br />
+        <Link to="/component/pages/Contact">Contact</Link>
+        <br />
+      </div>
+      <Switch>
+        <Route exact path="/component/pages/Home">
+          <Home />
+        </Route>
+        <Route path="/component/pages/Profile">
+          <Profile />
+        </Route>
+        <Route path="/component/pages/Skill">
+          <Skill />
+        </Route>
+        <Route path="/component/pages/Product">
+          <Product />
+        </Route>
+        <Route path="/component/pages/Contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
