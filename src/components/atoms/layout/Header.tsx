a2f2043fdb3581239f7de9c9677f4880209679ad
it-tsumugi <tsumugi.it@gmail.com>
2021-07-03 23:28:36 +0900

    ブランチを分ける

--- src/components/atoms/layout/Header.tsx
+++ src/components/atoms/layout/Header.tsx
@@ -1,39 +1,80 @@
 import styled from "styled-components";
-import {FC} from "react";
+import React,{FC} from "react";
 import { Link } from "react-router-dom";
 
+import { Tab, Tabs, AppBar } from "@material-ui/core";
 
-export const Path = {
-    home: "/",
-    profile: "/profile",
-    skill: "/skill",
-    product: "/product",
-    study: "/book",
-    contact: "/contact",
-  };
+interface headerItemPropsType{
+  id: number; 
+  route: string; 
+  label: string 
+}
+const headerItem: headerItemPropsType[] = [
+  {
+    id: 1,
+    route: "/",
+    label: "HOME",
+  },
+  {
+    id: 2,
+    route: "/profile",
+    label: "PROFILE",
+  },
+  {
+    id: 3,
+    route: "/skill",
+    label: "SKILL",
+  },
+  {
+    id: 4,
+    route: "/product",
+    label: "PRODUCT",
+  },
+  {
+    id: 5,
+    route: "/book",
+    label: "STUDY",
+  },
+  {
+    id: 6,
+    route: "/contact",
+    label: "CONTACT",
+  },
+];
 
 export const Header: FC = () => {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
   return (
-    <SHeader>
-      <SLink to={Path.home}>Home</SLink>
-      <SLink to={Path.profile}>Profile</SLink>
-      <SLink to={Path.skill}>Skill</SLink>
-      <SLink to={Path.product}>Product</SLink>
-      <SLink to={Path.study}>Book</SLink>
-      <SLink to={Path.contact}>Contact</SLink>
-    </SHeader>
+      <SAppBar position="static" color="inherit">
+        {/*ここがあやしい */}
+        <Tabs
+          value={value}
+          variant="fullWidth"
+          onChange={handleChange}
+          indicatorColor="secondary"
+          centered
+          aria-label="simple tabs example"
+        >
+          {headerItem.map((val) => {
+            return (
+              <Tab
+                label={val.label}
+                value={val.route}
+                key={val.id}
+                component={Link}
+                to={val.route}
+              />
+            );
+          })}
+        </Tabs>
+      </SAppBar>
   );
 };
-
-const SHeader = styled.header`
-    background-color: #193278;
-    width: 100%;
-    color: #fff;
-    text-align: center;
-    padding: 8px 0;
-`;
-
-const SLink = styled(Link)`
-    margin: 0 8px;
-    color: white;
+const SAppBar = styled(AppBar)`
+  background: #193278;
+  color: #fff;
+  padding: 8px 0;
 `;
